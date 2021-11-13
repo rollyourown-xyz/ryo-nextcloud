499f5ca665b4374f21d38ee9f0a888c8b323bde3
xyzroller <xyzroller@rollyourown.xyz>
2021-11-13 22:22:45 +0100

    Removed redundant wellknown configuration

--- project-deployment/04-deploy-wellknown-configuration.tf
+++ project-deployment/04-deploy-wellknown-configuration.tf
@@ -16,18 +16,6 @@ module "deploy-nextcloud-wellknown-configuration" {
       wellknown_domain       = local.project_nextcloud_domain_name,
       wellknown_path         = "/.well-known/caldav",
       wellknown_redirect_url = join("", [ "https://", local.project_nextcloud_domain_name, "/remote.php/dav" ])
-    }#,
-    
-    # nextcloud-nodeinfo = {
-    #   wellknown_domain       = local.project_nextcloud_domain_name,
-    #   wellknown_path         = "/.well-known/webfinger",
-    #   wellknown_redirect_url = join("", [ "https://", local.project_nextcloud_domain_name, "/.well-known/nodeinfo" ])
-    # },
-    
-    # nextcloud-webfinger = {
-    #   wellknown_domain       = local.project_nextcloud_domain_name,
-    #   wellknown_path         = "/.well-known/nodeinfo",
-    #   wellknown_redirect_url = join("", [ "https://", local.project_nextcloud_domain_name, "/index.php/.well-known/webfinger" ])
-    # }
+    }
   }
 }
