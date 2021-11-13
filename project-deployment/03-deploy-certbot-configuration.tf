bc7f3bd29a867361c31ff3dccffbb0239ae8abff
xyzroller <xyzroller@rollyourown.xyz>
2021-11-13 11:50:06 +0100

    Added certbot configuration deployment

---
+++ project-deployment/03-deploy-certbot-configuration.tf
@@ -0,0 +1,10 @@
+# Deploy certbot configuration for nextcloud server
+###################################################
+
+module "deploy-nextcloud-cert-domains" {
+  source = "../../ryo-service-proxy/module-deployment/modules/deploy-cert-domains"
+
+  certificate_domains = {
+    domain_1 = {domain = local.project_nextcloud_domain_name, admin_email = local.project_admin_email},
+  }
+}
