27da607e557c4f6b67763df681d8a804086baeb2
xyzroller <xyzroller@rollyourown.xyz>
2021-11-13 21:28:58 +0100

    Fixed domain name

--- project-deployment/04-deploy-wellknown-configuration.tf
+++ project-deployment/04-deploy-wellknown-configuration.tf
@@ -9,25 +9,25 @@ module "deploy-nextcloud-wellknown-configuration" {
     nextcloud-carddav = {
       wellknown_domain       = local.project_nextcloud_domain_name,
       wellknown_path         = "/.well-known/carddav",
-      wellknown_redirect_url = join("", [ "https://", local.project_domain_name, "/remote.php/dav" ])
+      wellknown_redirect_url = join("", [ "https://", local.project_nextcloud_domain_name, "/remote.php/dav" ])
     },
     
     nextcloud-caldav = {
       wellknown_domain       = local.project_nextcloud_domain_name,
       wellknown_path         = "/.well-known/caldav",
-      wellknown_redirect_url = join("", [ "https://", local.project_domain_name, "/remote.php/dav" ])
+      wellknown_redirect_url = join("", [ "https://", local.project_nextcloud_domain_name, "/remote.php/dav" ])
     }#,
     
     # nextcloud-nodeinfo = {
     #   wellknown_domain       = local.project_nextcloud_domain_name,
     #   wellknown_path         = "/.well-known/webfinger",
-    #   wellknown_redirect_url = join("", [ "https://", local.project_domain_name, "/.well-known/nodeinfo" ])
+    #   wellknown_redirect_url = join("", [ "https://", local.project_nextcloud_domain_name, "/.well-known/nodeinfo" ])
     # },
     
     # nextcloud-webfinger = {
     #   wellknown_domain       = local.project_nextcloud_domain_name,
     #   wellknown_path         = "/.well-known/nodeinfo",
-    #   wellknown_redirect_url = join("", [ "https://", local.project_domain_name, "/index.php/.well-known/webfinger" ])
+    #   wellknown_redirect_url = join("", [ "https://", local.project_nextcloud_domain_name, "/index.php/.well-known/webfinger" ])
     # }
   }
 }
