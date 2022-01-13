ed9a60d8fde1b6173a6dfb3964e8ca488fbb222c
xyzroller <xyzroller@rollyourown.xyz>
2022-01-13 17:50:15 +0100

    Consistent mount directory and ingress-proxy names

---
+++ project-deployment/06-deploy-ingress-proxy-configuration-nextcloud.tf
@@ -0,0 +1,28 @@
+# Deploy Ingress Proxy configuration for nextcloud
+##################################################
+
+module "deploy-nextcloud-ingress-proxy-backend-service" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-backend-services"
+
+  non_ssl_backend_services     = [ "nextcloud" ]
+}
+
+module "deploy-nextcloud-ingress-proxy-acl-configuration" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
+
+  depends_on = [ module.deploy-nextcloud-ingress-proxy-backend-service ]
+
+  ingress-proxy_host_only_acls = {
+    host-nextcloud = {host = local.project_nextcloud_domain_name}
+  }
+}
+
+module "deploy-nextcloud-ingress-proxy-backend-configuration" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
+
+  depends_on = [ module.deploy-nextcloud-ingress-proxy-acl-configuration ]
+
+  ingress-proxy_acl_use-backends = {
+    host-nextcloud = {backend_service = "nextcloud"}
+  }
+}
