d39c33206e831050dcdf3d808a0b5187452ecd48
xyzroller <xyzroller@rollyourown.xyz>
2022-01-16 18:58:20 +0100

    Add skip_modules and module deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ helpMessage()
   echo "Flags:"
   echo -e "-n hostname \t\t(Mandatory) Name of the host on which to deploy the project"
   echo -e "-v version \t\t(Mandatory) Version stamp for images to deploy, e.g. 20210101-1"
+  echo -e "-s \t\t\tSkip module deployment (should only be used during a restore after system failure)"
   echo -e "-h \t\t\tPrint this help message"
   echo ""
   exit 1
@@ -33,11 +34,14 @@ errorMessage()
 # Command-line input handling
 #############################
 
-while getopts n:v:h flag
+skip_modules="false"
+
+while getopts n:v:sh flag
 do
   case "${flag}" in
     n) hostname=${OPTARG};;
     v) version=${OPTARG};;
+    s) skip_modules="true";;
     h) helpMessage ;;
     ?) errorMessage ;;
   esac
@@ -69,85 +73,27 @@ git pull
 # Deploy Modules
 ################
 
-# Get user input for whether to deploy all modules (default yes)
-echo ""
-echo "Deploy all modules?"
-echo -n "If this is the first project to be deployed on the host "$hostname", then answer 'y' (the default) "
-read -e -p "[y/n]:" DEPLOY_MODULES
-DEPLOY_MODULES="${DEPLOY_MODULES:-"y"}"
-DEPLOY_MODULES="${DEPLOY_MODULES,,}"
+# If -s flag is not included, get user input for whether to deploy all modules (default yes)
 
-if [ ! "$DEPLOY_MODULES" == "y" ] && [ ! "$DEPLOY_MODULES" == "n" ]; then
-  echo "Invalid option "${DEPLOY_MODULES}". Quitting"
-  exit 1
+if [ $skip_modules == false ]; then
 
-elif [ "$DEPLOY_MODULES" == "y" ]; then
-  echo "Deploying all modules."
-  for module in $MODULES
-  do
+  echo ""
+  echo "Deploy all modules?"
+  echo -n "If this is the first project to be deployed on the host "$hostname", then answer 'y' (the default) "
+  read -e -p "[y/n]:" DEPLOY_MODULES
+  DEPLOY_MODULES="${DEPLOY_MODULES:-"y"}"
+  DEPLOY_MODULES="${DEPLOY_MODULES,,}"
 
-    # Clone or update module
-    echo ""
-    if [ -d ""$SCRIPT_DIR"/../"$module"" ]
-    then
-      echo "Module "$module" already cloned to this control node, refreshing with git pull"
-      cd "$SCRIPT_DIR"/../"$module" && git pull
-    else
-      echo "Cloning "$module" repository. Executing 'git clone' for "$module" repository"
-      #git clone https://github.com/rollyourown-xyz/"$module" "$SCRIPT_DIR"/../"$module"
-      git clone https://git.rollyourown.xyz/ryo-projects/"$module" "$SCRIPT_DIR"/../"$module"
-    fi
+  if [ ! "$DEPLOY_MODULES" == "y" ] && [ ! "$DEPLOY_MODULES" == "n" ]; then
+    echo "Invalid option "${DEPLOY_MODULES}". Quitting"
+    exit 1
 
-    # Run host setup playbooks for module
-    echo ""
-    if [ -f ""$SCRIPT_DIR"/../"$module"/configuration/"$hostname"_playbooks_executed" ]
-    then
-      echo "Host setup for "$module" module has already been done on "$hostname""
-    else
-      echo "Running module-specific host setup for "$module" on "$hostname""
-      "$SCRIPT_DIR"/../"$module"/scripts-module/host-setup.sh -n "$hostname"
-    fi
-
-    # Run packer image build for module
-    echo ""
-    echo "Running build-images script for "$module" module on "$hostname" with version "$version""
-    "$SCRIPT_DIR"/../"$module"/scripts-module/build-images.sh -n "$hostname" -v "$version"
-
-    # Deploy module
-    echo ""
-    echo "Deploying image(s) "$module" module on "$hostname" using images with version "$version""
-    "$SCRIPT_DIR"/../"$module"/scripts-module/deploy-module.sh -n "$hostname" -v "$version"
-
-  done
-
-else
-  echo "Checking for each module."
-  for module in $MODULES
-  do
-    # Get user input for whether to do module deployment (default yes)
-    echo ""
-    echo "Checking whether to deploy "$module" module."
-    echo "Check the documentation for already-deployed projects at https://rollyourown.xyz/rollyourown/projects/"
-    echo "If this module has already been deployed for another project on the host "$hostname", then answer 'n'."
-    echo "Default is 'y'."
-    echo -n "Deploy "$module" module? "
-    read -e -p "[y/n]:" DEPLOY_MODULE
-    DEPLOY_MODULE="${DEPLOY_MODULE:-"y"}"
-    DEPLOY_MODULE="${DEPLOY_MODULE,,}"
-    
-    # Check input
-    while [ ! "$DEPLOY_MODULE" == "y" ] && [ ! "$DEPLOY_MODULE" == "n" ]
+  elif [ "$DEPLOY_MODULES" == "y" ]; then
+    echo "Deploying all modules."
+    for module in $MODULES
     do
-      echo "Invalid option "${DEPLOY_MODULE}". Please try again."
-      echo -n "Deploy "$module" module (default is 'y')? "
-      read -e -p "[y/n]:" DEPLOY_MODULE
-      DEPLOY_MODULE="${DEPLOY_MODULE:-"y"}"
-      DEPLOY_MODULE="${DEPLOY_MODULE,,}"
-    done
 
-    if [ "$DEPLOY_MODULE" == "y" ]; then
-
-      # Clone or update module repository
+      # Clone or update module
       echo ""
       if [ -d ""$SCRIPT_DIR"/../"$module"" ]
       then
@@ -159,33 +105,64 @@ else
         git clone https://git.rollyourown.xyz/ryo-projects/"$module" "$SCRIPT_DIR"/../"$module"
       fi
 
-      # Run host setup playbooks for module
-      echo ""
-      if [ -f ""$SCRIPT_DIR"/../"$module"/configuration/"$hostname"_playbooks_executed" ]
-      then
-        echo "Host setup for "$module" module has already been done on "$hostname""
-      else
-        echo "Running module-specific host setup for "$module" on "$hostname""
-        "$SCRIPT_DIR"/../"$module"/scripts-module/host-setup.sh -n "$hostname"
-      fi
-
-      # Run packer image build for module
-      echo ""
-      echo "Running build-images script for "$module" module on "$hostname" with version "$version""
-      "$SCRIPT_DIR"/../"$module"/scripts-module/build-images.sh -n "$hostname" -v "$version"
-
       # Deploy module
       echo ""
-      echo "Deploying image(s) "$module" module on "$hostname" using images with version "$version""
-      "$SCRIPT_DIR"/../"$module"/scripts-module/deploy-module.sh -n "$hostname" -v "$version"
+      echo "Deploying "$module" module on "$hostname" with version "$version""
+      /bin/bash "$SCRIPT_DIR"/../"$module"/deploy.sh -n "$hostname" -v "$version"
 
-    else
+    done
+
+  else
+    echo "Checking for each module."
+    for module in $MODULES
+    do
+      # Get user input for whether to do module deployment (default yes)
       echo ""
-      echo "Skipping "$module" module deployment."
-    fi
-  done
-fi
+      echo "Checking whether to deploy "$module" module."
+      echo "Check the documentation for already-deployed projects at https://rollyourown.xyz/rollyourown/projects/"
+      echo "If this module has already been deployed for another project on the host "$hostname", then answer 'n'."
+      echo "Default is 'y'."
+      echo -n "Deploy "$module" module? "
+      read -e -p "[y/n]:" DEPLOY_MODULE
+      DEPLOY_MODULE="${DEPLOY_MODULE:-"y"}"
+      DEPLOY_MODULE="${DEPLOY_MODULE,,}"
+      
+      # Check input
+      while [ ! "$DEPLOY_MODULE" == "y" ] && [ ! "$DEPLOY_MODULE" == "n" ]
+      do
+        echo "Invalid option "${DEPLOY_MODULE}". Please try again."
+        echo -n "Deploy "$module" module (default is 'y')? "
+        read -e -p "[y/n]:" DEPLOY_MODULE
+        DEPLOY_MODULE="${DEPLOY_MODULE:-"y"}"
+        DEPLOY_MODULE="${DEPLOY_MODULE,,}"
+      done
 
+      if [ "$DEPLOY_MODULE" == "y" ]; then
+
+        # Clone or update module repository
+        echo ""
+        if [ -d ""$SCRIPT_DIR"/../"$module"" ]
+        then
+          echo "Module "$module" already cloned to this control node, refreshing with git pull"
+          cd "$SCRIPT_DIR"/../"$module" && git pull
+        else
+          echo "Cloning "$module" repository. Executing 'git clone' for "$module" repository"
+          #git clone https://github.com/rollyourown-xyz/"$module" "$SCRIPT_DIR"/../"$module"
+          git clone https://git.rollyourown.xyz/ryo-projects/"$module" "$SCRIPT_DIR"/../"$module"
+        fi
+
+        # Deploy module
+        echo ""
+        echo "Deploying "$module" module on "$hostname" with version "$version""
+        /bin/bash "$SCRIPT_DIR"/../"$module"/deploy.sh -n "$hostname" -v "$version"
+
+      else
+        echo ""
+        echo "Skipping "$module" module deployment."
+      fi
+    done
+  fi
+fi
 
 # Deploy project components
 ###########################
