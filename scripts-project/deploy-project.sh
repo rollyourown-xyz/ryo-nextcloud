196d38828c23e8d17256fc33f382d84f8f067ae1
xyzroller <xyzroller@rollyourown.xyz>
2022-01-16 11:48:51 +0100

    Fix updating ryo-host deployed-projects file

--- scripts-project/deploy-project.sh
+++ scripts-project/deploy-project.sh
@@ -39,8 +39,14 @@ then
   errorMessage
 fi
 
+# Get Project ID from configuration file
+PROJECT_ID="$(yq eval '.project_id' "$SCRIPT_DIR"/../configuration/configuration_"$hostname".yml)"
 
-# Deploy project
+# Deploy components
+###################
+
+echo "Deploying components for "$PROJECT_ID" project on "$hostname""
+echo ""
 
 # Set up / switch to project workspace for host
 if [ -f ""$SCRIPT_DIR"/../configuration/"$hostname"_workspace_created" ]; then
@@ -57,9 +63,9 @@ else
    terraform -chdir="$SCRIPT_DIR"/../project-deployment workspace new "$hostname"
    touch "$SCRIPT_DIR"/../configuration/"$hostname"_workspace_created
 fi
-# Deploy
-echo ""
+
 echo "Executing command: terraform -chdir="$SCRIPT_DIR"/../project-deployment init"
+echo ""
 terraform -chdir="$SCRIPT_DIR"/../project-deployment init
 echo ""
 echo "Executing command: terraform -chdir="$SCRIPT_DIR"/../project-deployment apply -input=false -auto-approve -var \"host_id="$hostname"\" -var \"image_version="$version"\""
@@ -67,22 +73,32 @@ terraform -chdir="$SCRIPT_DIR"/../project-deployment apply -input=false -auto-ap
 echo "Completed"
 
 
-# Update ryo-host deployed-projects array for "$hostname"
+# Update ryo-host deployed-projects file for "$hostname"
+########################################################
 
-# Get Project ID from configuration file
-PROJECT_ID="$(yq eval '.project_id' "$SCRIPT_DIR"/../configuration/configuration_"$hostname".yml)"
+DEPLOYED_PROJECTS_FILE="$SCRIPT_DIR"/../../ryo-host/backup-restore/deployed-projects_"$hostname".yml
 
 # Check existence of deployed-projects file and create it not existing
-if [ ! -f "$SCRIPT_DIR"/../../ryo-host/backup-restore/deployed-projects_"$hostname".yml ]
-then
-  cp "$SCRIPT_DIR"/../../ryo-host/backup-restore/deployed-projects_TEMPLATE.yml "$SCRIPT_DIR"/../../ryo-host/backup-restore/deployed-projects_"$hostname".yml
+echo ""
+echo "Checking existence of deployed-projects file for "$hostname" and creating if not existing"
+
+if [ ! -f "$DEPLOYED_PROJECTS_FILE" ]; then
+  cp "$SCRIPT_DIR"/../../ryo-host/backup-restore/deployed-projects_TEMPLATE.yml "$DEPLOYED_PROJECTS_FILE"
 fi
 
-# Add project to deployed-project array for "$hostname"
-if [ ! yq eval '. |= any_c(. == "$PROJECT_ID")' "$SCRIPT_DIR"/../../ryo-host/backup-restore/deployed-projects_"$hostname".yml ]
-then
-  yq eval -i '. += "$PROJECT_ID"' "$SCRIPT_DIR"/../../ryo-host/backup-restore/deployed-projects_"$hostname".yml
+
+# Add project to deployed-projects file for "$hostname"
+echo ""
+echo "Adding project to deployed-projects file for "$hostname""
+
+if [ $(yq_project_id="$PROJECT_ID" yq eval '. |= any_c(. == strenv(yq_project_id))' "$DEPLOYED_PROJECTS_FILE") == true ]; then
+  echo ""$PROJECT_ID" is already recorded in the deployed-projects_"$hostname".yml file"
+else
+  yq_project_id="$PROJECT_ID" yq eval -i '. += strenv(yq_project_id)' "$DEPLOYED_PROJECTS_FILE"
 fi
 
-# Delete the null entry if it exists:
-yq eval -i 'del(.[] | select(. == null))' "$SCRIPT_DIR"/../../ryo-host/backup-restore/deployed-projects_"$hostname".yml
+
+# Delete the null entry if it exists
+echo ""
+echo "Deleting the null entry if it exists"
+yq eval -i 'del(.[] | select(. == null))' "$DEPLOYED_PROJECTS_FILE"
