113af371bf9a90b969ddf948f7294c4b02b196aa
xyzroller <xyzroller@rollyourown.xyz>
2022-02-03 23:50:48 +0100

    Fix missing "

--- scripts-project/delete-project-containers.sh
+++ scripts-project/delete-project-containers.sh
@@ -48,7 +48,7 @@ lxc delete --force "$hostname":nextcloud
 echo ""
 
 echo "...deleting project container persistent storage"
-ansible-playbook -i "$SCRIPT_DIR"/../../ryo-host/configuration/inventory_"$hostname" "$SCRIPT_DIR"/../backup-restore/delete-project-persistent-storage.yml --extra-vars "project_id="$PROJECT_ID" host_id="$hostname"
+ansible-playbook -i "$SCRIPT_DIR"/../../ryo-host/configuration/inventory_"$hostname" "$SCRIPT_DIR"/../backup-restore/delete-project-persistent-storage.yml --extra-vars "project_id="$PROJECT_ID" host_id="$hostname""
 echo ""
 
 echo "Project containers deleted"
