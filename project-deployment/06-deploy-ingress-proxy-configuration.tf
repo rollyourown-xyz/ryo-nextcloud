b42614302acbdca68a0ec2ee0fcc4eee2af0875b
xyzroller <xyzroller@rollyourown.xyz>
2022-02-04 17:00:08 +0100

    Separate acl, deny and use-backend deployment

--- project-deployment/06-deploy-ingress-proxy-configuration.tf
+++ project-deployment/06-deploy-ingress-proxy-configuration.tf
@@ -9,7 +9,7 @@ module "deploy-nextcloud-ingress-proxy-backend-service" {
   non_ssl_backend_services     = [ "nextcloud" ]
 }
 
-module "deploy-nextcloud-ingress-proxy-configuration" {
+module "deploy-nextcloud-ingress-proxy-acl-configuration" {
   source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
 
   depends_on = [ module.deploy-nextcloud-ingress-proxy-backend-service ]
@@ -17,6 +17,12 @@ module "deploy-nextcloud-ingress-proxy-configuration" {
   ingress-proxy_host_only_acls = {
     host-nextcloud = {host = local.project_nextcloud_domain_name}
   }
+}
+
+module "deploy-nextcloud-ingress-proxy-use-backend-configuration" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
+
+  depends_on = [ module.deploy-nextcloud-ingress-proxy-backend-service, module.deploy-nextcloud-ingress-proxy-acl-configuration ]
 
   ingress-proxy_acl_use-backends = {
     host-nextcloud = {backend_service = "nextcloud"}
